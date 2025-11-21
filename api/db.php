955bcec4424434598afa54eedd1ea4b76b7a937b
Nam Admin <148678363+TranTrungNamn@users.noreply.github.com>
2025-11-21 22:21:15 +0700

    db.php update

--- api/db.php
+++ api/db.php
@@ -1,14 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "web_scraping_002";
-$port = 3306;
+$host   = "localhost";
+$user   = "sql_nhom30_itimi";
+$pass   = "71bbaeb8a35948";
+$dbname = "sql_nhom30_itimi";
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-$conn->set_charset("utf8mb4");
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
-    die("Kết nối MySQL thất bại: " . $conn->connect_error);
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
+
+mysqli_set_charset($conn, "utf8");
 ?>
